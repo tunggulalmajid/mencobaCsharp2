3d39c26a267a58d4896b215c22c11866a04fcc69
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-28 22:43:32 +0700

    Add project files.

---
+++ mencoba csharp 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mencoba_csharp_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int Jumlah = kali(a, b);
+            Console.WriteLine($"hasil kali dari {a} dan {b} adalah {Jumlah}");
+            Console.ReadLine();
+        }
+        static int kali(int a, int b)
+        {
+            int Jumlah = a * b;
+            return Jumlah;
+        }
+
+    }
+}
